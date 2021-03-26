08c6e2e2d31bce5cff2581179f3ee16e24c6350e
Andrew Findon <>
2021-03-26 16:55:55 +0000

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <initializer_list>
+//#include <iostream>
 
 void freeze_up()
 {
-	auto bad_var = {1, 2, 3};
-}
+	auto bad_var = {1, 2, 3};		// A breakpoint here will trigger the issue.
+}									// A breakpoint here is fine.
 
 int main() {
+	//std::cout << "freeze test" << std::endl;
+
 	freeze_up();
 	
 	return 0;
